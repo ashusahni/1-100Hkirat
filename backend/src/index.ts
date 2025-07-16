94db53d99929f249a3dd8176b70c5fd54579e795
ashutosh <ashutoshsahni833@gmail.com>
2025-07-16 22:39:42 +0530

    otp_fix

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -9,17 +9,33 @@ const otpStore: Record<string, string> = {};
 
 //endpoint to generate otp 
 app.get("generateOtp",(req,res)=>{
- const email = req.body.email
- if(!email){
-  return res.status(400).json({msg:"email is required"})
- }
-
+  const email = req.body.email
+  if(!email){
+    return res.status(400).json({msg:"email is required"})
+  }
+  
   const otp = Math.floor(100000 + Math.random() * 900000).toString(); // generates a 6-digit OTP
   otpStore[email] = otp;
- console.log(`OTP for ${email}: ${otp}`)
- res.status(200).json({msg:"otp generated and logged"})
-
+  console.log(`OTP for ${email}: ${otp}`)
+  res.status(200).json({msg:"otp generated and logged"})
+  
 })
+//endpoint to reset password
+app.post("/reset-password",(req,res)=>{
+  const {email,otp,newPassword} = req.body;
+  if(!email || !otp || !newPassword){
+    return res.status(400).json({msg:"otp,email,password"})
+  }
+
+if(otpStore[email]===otp){
+  console.log(`Password for ${email} has been reset to: ${newPassword}`)
+  delete otpStore[email]
+  res.status(200).json({msg:"password has been reset successfully"})
+}else{
+  res.status(400).json({msg:"invalid otp"})
+}
+})
+
 
 
 
